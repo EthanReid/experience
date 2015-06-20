a732204b69c8734b9f7b809add3cf9e9bb7b432a
Ethan Reid <erpreid@gmail.com>
2015-06-20 10:09:05 -0700

    Up
    Up

---
+++ Assets/Next To Mars.js
@@ -0,0 +1,13 @@
+﻿#pragma strict
+
+function Start () {
+	LoadAfterDelay("Mars");
+}
+ function LoadAfterDelay(Mars: String){
+   yield WaitForSeconds(11); // wait 10 seconds
+   Application.LoadLevel(Mars);
+ }
+    
+function Update () {
+
+}
